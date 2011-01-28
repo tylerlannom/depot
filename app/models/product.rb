492f6492ff94ca7f89b7af7f70619b2c121f6d48
Tyler Lannom <glannom@mac.com>
2011-01-28 15:45:02 -0500

    Start of Catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	
+	default_scope :order => 'title'
+	
 	validates :title, :uniqueness => true
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -6,4 +9,5 @@ class Product < ActiveRecord::Base
 		:with		=>	%r{\.(gif|jpg|png)$}i,
 		:message => 'must be a URL for GIF, JPG of PNG image.'
 	}
+	
 end
