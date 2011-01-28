f35e686c4af702765f890c130b0e108a031c95cf
Tyler Lannom <glannom@mac.com>
2011-01-28 10:10:35 -0500

    product validation and unit testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title, :uniqueness => true
+	validates :title, :description, :image_url, :presence => true
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates :image_url, :format => {
+		:with		=>	%r{\.(gif|jpg|png)$}i,
+		:message => 'must be a URL for GIF, JPG of PNG image.'
+	}
 end
