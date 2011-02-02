0c0b443aafdfab5c97b1218bcddf336b9e215cd0
Tyler Lannom <glannom@mac.com>
2011-02-02 16:02:42 -0500

    first phase of checkout

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -47,7 +47,7 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should update line_item" do
     put :update, :id => @line_item.to_param, :line_item => @line_item.attributes
-    assert_redirected_to line_item_path(assigns(:line_item))
+    assert_redirected_to store_path
   end
 
   test "should destroy line_item" do
@@ -55,6 +55,6 @@ class LineItemsControllerTest < ActionController::TestCase
       delete :destroy, :id => @line_item.to_param
     end
 
-    assert_redirected_to line_items_path
+    assert_redirected_to store_path
   end
 end
